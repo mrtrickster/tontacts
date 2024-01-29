bdaf00a6f90efe4c32fd2ec989168ca6a717a953
Oleg R <oleg@rapty.app>
2024-01-29 13:14:16 +0700

    Resizing test.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const version = "0.1.13";
+const version = "0.1.14";
 
 const gendersList = { "": "", "m": "Male", "f": "Female", "x": "Other" };
 const countriesList = { "": "", "ru": "Russia", "th": "Thailand", "us": "USA", "ua": "Ukraine" };
