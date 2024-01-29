bb3e88f4b449fd96d41cf4cd7aa92b79308b79d6
Oleg R <oleg@rapty.app>
2024-01-29 12:04:45 +0700

    Canvas.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const version = "0.1.5";
+const version = "0.1.6";
 
 const gendersList = { "": "", "m": "Male", "f": "Female", "x": "Other" };
 const countriesList = { "": "", "ru": "Russia", "th": "Thailand", "us": "USA", "ua": "Ukraine" };
