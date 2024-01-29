53e7de3dd6f8acc0add97906a874e7c5ed5d742a
Oleg R <oleg@rapty.app>
2024-01-29 20:11:22 +0700

    Contact list styles. Edit contact bugfix.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const version = "0.1.30";
+const version = "0.1.31";
 
 const gendersList = { "": "", "m": "Male", "f": "Female", "x": "Other" };
 const countriesList = { "": "", "ru": "Russia", "th": "Thailand", "us": "USA", "ua": "Ukraine" };
