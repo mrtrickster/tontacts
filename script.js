24475a94f7bb46cb45825accdd3103cead14c02e
Oleg R <oleg@rapty.app>
2024-01-29 13:59:46 +0700

    Using jpeg instaed of png to minimize userpic size.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const version = "0.1.18";
+const version = "0.1.19";
 
 const gendersList = { "": "", "m": "Male", "f": "Female", "x": "Other" };
 const countriesList = { "": "", "ru": "Russia", "th": "Thailand", "us": "USA", "ua": "Ukraine" };
