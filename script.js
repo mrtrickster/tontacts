646facb82b0d037010564e508e78dffc296cd5ca
Oleg R <oleg@rapty.app>
2024-01-30 10:34:58 +0700

    Menu dropdown button added.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const version = "0.1.33";
+const version = "0.1.34";
 
 const gendersList = { "": "", "m": "Male", "f": "Female", "x": "Other" };
 const countriesList = { "": "", "ru": "Russia", "th": "Thailand", "us": "USA", "ua": "Ukraine" };
