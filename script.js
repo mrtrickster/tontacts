6cf2a2d0cf4bb6427b791ca9ebc11d55da7c6723
Oleg R <oleg@rapty.app>
2024-01-30 15:56:15 +0700

    Stylings.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const version = "0.1.48";
+const version = "0.1.49";
 
 const gendersList = { "": "", "m": "Male", "f": "Female", "x": "Other" };
 const countriesList = { "": "", "ru": "Russia", "th": "Thailand", "us": "USA", "ua": "Ukraine" };
